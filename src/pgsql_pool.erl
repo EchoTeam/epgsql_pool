794fb6e0490436cbd64e957dfe53a8852c88b443
snaky <snaky@aboutecho.com>
2012-09-13 20:20:05 +0400

    removed epgsql_pool_log.erl

--- src/pgsql_pool.erl
+++ src/pgsql_pool.erl
@@ -29,7 +29,6 @@ start_link(Size, Opts) ->
 start_link(undefined, Size, Opts) ->
     start_link(Size, Opts);
 start_link(Name, Size, Opts) ->
-    epgsql_pool_log:log_info(?MODULE, "[start_link] Starting pgsql pool: ~p of size: ~p", [Name, Size]),
     gen_server:start_link({local, Name}, ?MODULE, {Name, Size, opts(Opts)}, []).
 
 %% @doc Stop the pool, close all db connections
@@ -77,8 +76,7 @@ init({Name, Size, Opts}) ->
     Connections = case connect(Opts) of
          {ok, Connection} ->
              [{Connection, now_secs()}];
-         {error, Error} ->
-             epgsql_pool_log:log_error(?MODULE, "Failed to connect to PostgreSQL server. Error:~n~p~n", [Error]),
+         {error, _Error} ->
              []
      end,
     {ok, TRef} = timer:send_interval(60000, close_unused),
@@ -101,31 +99,23 @@ handle_call(get_connection_nowait, From, State) ->
     handle_call({get_connection, nowait}, From, State);
 
 handle_call({get_connection, WaitMode}, From, #state{connections = Connections, waiting = Waiting} = State) ->
-    epgsql_pool_log:log_debug(?MODULE, "[handle_call get_connection] WaitMode: ~p", [WaitMode]),
     case Connections of
         [{C, _} | T] -> 
-            epgsql_pool_log:log_debug(?MODULE, "[handle_call get_connection] Return existing unused connection"),
             {noreply, deliver(From, C, State#state{connections = T})};
         [] ->
-            epgsql_pool_log:log_debug(?MODULE, "[handle_call get_connection] No free connections."),
             case length(State#state.monitors) < State#state.size of
                 true ->
-                    epgsql_pool_log:log_debug(?MODULE, "[handle_call get_connection] Allocate a new connection and return it."),
                     case connect(State#state.opts) of
                         {ok, C} ->
-                            epgsql_pool_log:log_debug(?MODULE, "[handle_call get_connection] Allocated new connection: ~p", [C]),
                             {noreply, deliver(From, C, State)};
-                        {error, Error} ->
-                            epgsql_pool_log:log_error(?MODULE, "[handle_call get_connection] Got error: ~p; return {error, busy}", [Error]),
+                        {error, _Error} ->
                             {reply, {error, busy}, State}
                     end;
                 false ->
                     case WaitMode of
                         wait ->
-                            epgsql_pool_log:log_error(?MODULE, "[handle_call get_connection] Reached max connections, let the requestor wait."),
                             {noreply, State#state{waiting = queue:in(From, Waiting)}};
                         nowait ->
-                            epgsql_pool_log:log_error(?MODULE, "[handle_call get_connection] Reached max connections, return {error, busy}"),
                             {reply, {error, busy}, State}
                     end
             end
@@ -147,11 +137,9 @@ handle_call(Request, _From, State) ->
 handle_cast({return_connection, C}, #state{monitors = Monitors} = State) ->
     case lists:keytake(C, 1, Monitors) of
         {value, {C, M}, Monitors2} ->
-            epgsql_pool_log:log_debug(?MODULE, "[handle_cast return_connection] Found connection ~p in the monitors list", [C]),
             erlang:demonitor(M),
             {noreply, return(C, State#state{monitors = Monitors2})};
         false ->
-            epgsql_pool_log:log_debug(?MODULE, "[handle_cast return_connection] Have not found connection ~p in the monitors list", [C]),
             {noreply, State}
     end;
 
@@ -181,30 +169,24 @@ handle_info(never_close_unused, State) ->
 
 %% Requestor we are monitoring went down. Kill the associated connection, as it might be in an unknown state.
 handle_info({'DOWN', M, process, _Pid, _Info}, #state{monitors = Monitors} = State) ->
-    epgsql_pool_log:log_debug(?MODULE, "[handle_info DOWN] Requestor we are monitoring went down. monitor: ~p", [M]),
     case lists:keytake(M, 2, Monitors) of
         {value, {C, M}, Monitors2} ->
-            epgsql_pool_log:log_debug(?MODULE, "[handle_info DOWN] closed connection, deleted the monitor from the monitors list."),
             catch pgsql:close(C),
             {noreply, State#state{monitors = Monitors2}};
         false ->
-            epgsql_pool_log:log_debug(?MODULE, "[handle_info DOWN] Have not found the monitor in the monitors list"),
             {noreply, State}
     end;
 
 %% One of our database connections went down. Clean up our administration.
 handle_info({'EXIT', ConnectionPid, _Reason}, State) ->
-    epgsql_pool_log:log_debug(?MODULE, "[handle_info EXIT] One of our database connections went down. Clean up our administration. Pid: ~p", [ConnectionPid]),
     #state{connections = Connections, monitors = Monitors} = State,
     Connections2 = proplists:delete(ConnectionPid, Connections),
     F = fun({C, M}) when C == ConnectionPid ->
-            epgsql_pool_log:log_debug(?MODULE, "[handle_info EXIT] demonitor the requestor connected via the closed connection. M: ~p", [M]),
             erlang:demonitor(M),
             false;
         ({_, _}) -> true
     end,
     Monitors2 = lists:filter(F, Monitors),
-    epgsql_pool_log:log_debug(?MODULE, "[handle_info EXIT] Connections: ~p; Monitors: ~p", [Connections2, Monitors2]),
     {noreply, State#state{connections = Connections2, monitors = Monitors2}};
 
 %% Trap unsupported info calls.
@@ -213,7 +195,6 @@ handle_info(Info, State) ->
 
 terminate(_Reason, State) ->
     timer:cancel(State#state.timer),
-    epgsql_pool_log:log_info(?MODULE, "[terminate] ~p terminating...", [State#state.id]),
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
@@ -228,34 +209,26 @@ connect(Opts) ->
     try
         connect_ll(Host, Username, Password, Opts)
     catch C:R ->
-        epgsql_pool_log:log_error(?MODULE, "[connect] Failed to connect to PostgreSQL server. Error:~n~p:~p", [C, R]),
         {error, {C, R}}
     end.
 
 connect_ll(Host, Username, Password, Opts) ->
-    epgsql_pool_log:log_debug(?MODULE, "[connect_ll] Connect to: ~p", [Host]),
     {ok, Conn} = pgsql:connect(Host, Username, Password, Opts),
-    epgsql_pool_log:log_debug(?MODULE, "[connect_ll] Got connection: ~p", [Conn]),
     {ok, [], []} = pgsql:squery(Conn, "SET search_path TO " ++ proplists:get_value(schema, Opts)),
-    epgsql_pool_log:log_debug(?MODULE, "[connect_ll] Return connection"),
     {ok, Conn}.
 
 deliver({Pid,_Tag} = From, C, #state{monitors=Monitors} = State) ->
     M = erlang:monitor(process, Pid),
     gen_server:reply(From, {ok, C}),
-    epgsql_pool_log:log_debug(?MODULE, "[deliver] Connection: ~p; New monitor: ~p", [C, M]),
     State#state{ monitors=[{C, M} | Monitors] }.
 
 return(C, #state{connections = Connections, waiting = Waiting} = State) ->
-    epgsql_pool_log:log_debug(?MODULE, "[return] return connection: ~p", [C]),
     case queue:out(Waiting) of
         {{value, From}, Waiting2} ->
-            epgsql_pool_log:log_debug(?MODULE, "[return] got a pending request from the wait list: ~p, deliver the connection to it", [From]),
             State2 = deliver(From, C, State),
             State2#state{waiting = Waiting2};
         {empty, _Waiting} ->
             Connections2 = Connections ++ [{C, now_secs()}],
-            epgsql_pool_log:log_debug(?MODULE, "[return] returned the connection to the connections pool, bumped last usage time"),
             State#state{connections = Connections2}
     end.
 
